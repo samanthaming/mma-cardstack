a1c590adb361f027d93ffeb9613ce626edf0fef6
Samantha Ming <samanthaming.ming@gmail.com>
2018-05-21 22:34:51 -0700

    Restructure directory. Add subtitle.

---
+++ scripts/data.js
@@ -0,0 +1,26 @@
+var data = [
+  {
+    id: 'ronda',
+    name: 'Ronda Rousey',
+    aka: 'Rowdy',
+    division: 'Bantamweight'
+  },
+  {
+    id: 'rose',
+    name: 'Rose Namajunas',
+    aka: 'Thug Rose',
+    divison: 'Strawweight'
+  },
+  {
+    id: 'holly',
+    name: 'Holly Holm',
+    aka: `The Preacher's Daughter`,
+    division: 'Featherweight '
+  },
+  {
+    id: 'joanna',
+    name: 'Joanna Jędrzejczyk',
+    aka: 'JJ',
+    division: 'Strawweight'
+  }
+];
--- scripts/es6.js
+++ scripts/es6.js
@@ -1,30 +1,3 @@
-const data = [
-  {
-    id: 'ronda',
-    name: 'Ronda Rousey',
-    aka: 'Rowdy',
-    division: 'Bantamweight'
-  },
-  {
-    id: 'rose',
-    name: 'Rose Namajunas',
-    aka: 'Thug Rose',
-    divison: 'Strawweight'
-  },
-  {
-    id: 'holly',
-    name: 'Holly Holm',
-    aka: `The Preacher's Daughter`,
-    division: 'Featherweight '
-  },
-  {
-    id: 'joanna',
-    name: 'Joanna Jędrzejczyk',
-    aka: 'JJ',
-    division: 'Strawweight'
-  }
-]
-
 const cardStack = document.querySelector('.card-stack');
 
 const cards = data.map((athlete, index) => {
